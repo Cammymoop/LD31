4ad65b24c098dcea0efb792c23cf1885fe95290f
meganyoon <megan.yoon1@gmail.com>
2014-12-06 12:33:01 -0600

    Megan's attempt at putting boxes in
    Okay so I added some lines for boxes, we'll see how it looks

--- js/Game.js
+++ js/Game.js
@@ -66,6 +66,17 @@ BaseNamespace.Game.prototype = {
         this.addBox(1270, 500);
         this.addBox(1268, 420);
         this.addBox(1267, 340);
+		
+		//this is where megan started writing code for boxes
+		this.addBox (1280, 500);
+		this.addBox (1300, 250);
+		this.addBox (1300, 300);
+		this.addBox (1300, 350);
+		this.addBox (1300, 400);
+		this.addBox (1300, 450);
+		this.addBox (1300, 500);
+		this.addBox (1300, 200);
+		//end of where megan started writing code for boxes
 
         this.frownie = this.game.add.sprite(this.game.world.centerX - 100, this.game.world.centerY, 'frownie');
 
