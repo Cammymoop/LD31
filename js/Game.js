e45b5f66684a6120d29eab53bae3aaa142904b7b
meganyoon <megan.yoon1@gmail.com>
2014-12-07 12:18:42 -0600

    fixing coins in lines #140-142

--- js/Game.js
+++ js/Game.js
@@ -137,9 +137,9 @@ BaseNamespace.Game.prototype = {
 		this.addCoin (addBoxOffset, 440);
 		addBoxOffset += 40;
 		this.addStack (addBoxOffset, ['smallBox', 'box1', 'box1']);
-		this.addCoin (addBoxOffset, 400);
-		this.addCoin (addBoxOffset, 360);
 		this.addCoin (addBoxOffset, 320);
+		this.addCoin (addBoxOffset, 280);
+		this.addCoin (addBoxOffset, 240);
 
 		this.addStack (addBoxOffset + 240, ['smallBox', 'scaffold', 'box1', 'box1']);
 
