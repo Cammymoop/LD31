ff5248bd8a9e943a8181221202b9e1215ed39dc2
Charles Burnham <felixtheslayer@gmail.com>
2014-12-07 18:16:07 -0600

    Add conveyor image

--- js/Game.js
+++ js/Game.js
@@ -29,6 +29,7 @@ BaseNamespace.Game.prototype = {
         this.physics.startSystem(Phaser.Physics.ARCADE);
         this.physics.arcade.gravity.y = 1400;
 
+        this.conveyor = this.game.add.sprite(this.game.world.centerX + 50, 560, 'conveyor');
         this.coins = this.add.group();
         this.boxes = this.add.group();
         this.enemyBuffer = this.add.group();
@@ -56,9 +57,9 @@ BaseNamespace.Game.prototype = {
         this.coinSound = this.add.audio('coinSFX');
         this.hurtSound = this.add.audio('hurtSFX');
 
-        this.conveyor = this.game.add.sprite(this.game.world.centerX + 60, 565, 'conveyor');
         this.conveyor.anchor.setTo(0.5, 0.5);
         this.physics.enable(this.conveyor, Phaser.Physics.ARCADE);
+        this.conveyor.body.setSize(756, 42);
         this.conveyor.body.allowGravity = false;
         this.conveyor.body.immovable = true;
 		
@@ -406,7 +407,7 @@ BaseNamespace.Game.prototype = {
 
         this.physics.arcade.overlap(player, this.coins, this.coinGrab, null, this);
 
-        this.boxes.callAll('boxUpdate', null, this.conveyor.body.x);
+        this.boxes.callAll('boxUpdate', null, this.conveyor.body.x + 10);
         this.enemies.callAll('enemyUpdate', null, this);
         var en = this.enemyBuffer.children.length;
         while (en--) {
